8f09fc6bc1eeae08b8d8ac38a16c34661ed806a6
yeymeap <bence3728@gmail.com>
2022-09-28 17:29:38 +0200

    name changes, color fixes

--- js/main.js
+++ js/main.js
@@ -22,13 +22,13 @@
     // Back to top button
     $(window).scroll(function () {
         if ($(this).scrollTop() > 100) {
-            $('.back-to-top').fadeIn('slow');
+            $('.back-to-top').fadeIn('fast');
         } else {
-            $('.back-to-top').fadeOut('slow');
+            $('.back-to-top').fadeOut('fast');
         }
     });
     $('.back-to-top').click(function () {
-        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        $('html, body').animate({scrollTop: 0}, 150, 'easeInOutExpo');
         return false;
     });
 
