9a169042c973e1f358c4fc1c61908a8e5a5bd2d2
yeymeap <bence3728@gmail.com>
2022-10-27 09:29:54 +0200

    added hover.js, updated prettier, added .prettierignore

--- js/hover.js
+++ js/hover.js
@@ -1,26 +1,25 @@
-var shopStart = $('#toc_container').offset().top;
-var shopStop = $('#stop').offset().top;
+var shopStart = $("#toc_container").offset().top;
+var shopStop = $("#stop").offset().top;
 var lastMode = "absolute";
 
-$(window).scroll(function() {
-  var mode;
-  if ($(this).scrollTop() >= shopStart) {
-    if ($(this).scrollTop() - shopStop + $('#toc_container').height() > 0)
-      mode = 'absolute';
-    else
-      mode = 'fixed';
-  } else {
-    mode = 'absolute';
-  }
+$(window).scroll(function () {
+	var mode;
+	if ($(this).scrollTop() >= shopStart) {
+		if ($(this).scrollTop() - shopStop + $("#toc_container").height() > 0)
+			mode = "absolute";
+		else mode = "fixed";
+	} else {
+		mode = "absolute";
+	}
 
-  if (lastMode !== mode) {
-    if (mode == 'fixed') {
-      $('#toc_container').css('position', 'fixed');
-      $('#toc_container').css('top', '0');
-    } else {
-      $('#toc_container').css('position', 'absolute');
-      $('#toc_container').css('top', shopStart);
-    }
-    lastMode = mode;
-  }
+	if (lastMode !== mode) {
+		if (mode == "fixed") {
+			$("#toc_container").css("position", "fixed");
+			$("#toc_container").css("top", "0");
+		} else {
+			$("#toc_container").css("position", "absolute");
+			$("#toc_container").css("top", shopStart);
+		}
+		lastMode = mode;
+	}
 });
