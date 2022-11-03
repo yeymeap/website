660537c7437df404d4ae31452e141c71e3b101c3
yeymeap <bence3728@gmail.com>
2022-11-03 10:39:16 +0100

    started making php pages

---
+++ footer.php
@@ -0,0 +1,51 @@
+        <!-- Footer Start -->
+        <div class="container-fluid bg-black text-white pt-5">
+            <div class="row px-xl-5 pt-0">
+                <div class="col-lg-4 col-md-12 mb-5 pe-3">
+                    <h5 class="text-white text-uppercase mb-4">Get In Touch With Us</h5>
+                    <p class="mb-4">If you have any questions, feel free to contact us!</p>
+                    <p class="mb-2"><i class="fa fa-map-marker-alt text-white me-3 pe-1"></i>Školská 938/6, Dunajská Streda, 929 01, Slovakia</p>
+                    <p class="mb-2"><i class="fa fa-envelope text-white me-3"></i>bence3728@gmail.com</p>
+                    <p class="mb-0"><i class="fa fa-phone-alt text-white me-3"></i>+421 949 131 222</p>
+                </div>
+                <div class="col-lg-8 col-md-12">
+                    <div class="row">
+                        <div class="col-md-4 mb-5"></div>
+                        <div class="col-md-4 mb-5">
+                            <h5 class="text-white text-uppercase mb-2 ms-5">Actions</h5>
+                            <div class="d-flex flex-column justify-content-start ms-5">
+                                <a class="text-white mb-2" href="index-en.html"><i class="fa fa-angle-right me-2"></i>Home</a>
+                                <a class="text-white mb-2" href="cart-en.html"><i class="fa fa-angle-right me-2"></i>Shopping Cart</a>
+                                <a class="text-white mb-2" href="checkout-en.html"><i class="fa fa-angle-right me-2"></i>Checkout</a>
+                                <a class="text-white" href="contact-en.html"><i class="fa fa-angle-right me-2"></i>Contact Us</a>
+                            </div>
+                        </div>
+                        <div class="col-md-4 mb-5">
+                            <h5 class="text-white text-uppercase mb-2 ms-5">Follow Us</h5>
+                            <div class="d-flex ms-5">
+                                <a class="btn btn-twitter btn-square me-2 text-decoration-none" href="https://twitter.com/yey_meap/" target="_blank"><i class="fab fa-twitter"></i></a>
+                                <a class="btn btn-facebook btn-square me-2 text-decoration-none" href="" target="_blank"><i class="fab fa-facebook-f"></i></a>
+                                <a class="btn btn-instagram btn-square text-decoration-none" href="https://www.instagram.com/botlobence/" target="_blank"><i class="fab fa-instagram"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row border-top mx-xl-5 py-4" style="border-color: rgba(256, 256, 256, .1) !important;">
+                <div class="col-md-6 px-xl-0">
+                    <p class="mb-md-0 text-center text-md-start text-white">
+                        &copy; All Rights Reserved. Designed by Bence Botló.
+                    </p>
+                </div>
+                <div class="col-md-6 px-xl-0 text-center text-md-end">
+                    <img class="img-fluid" src="img/payments.png" alt="">
+                </div>
+            </div>
+        </div>
+        <!-- Footer End -->
+
+
+        <!-- Back to Top -->
+        <a href="#" class="btn btn-backblack back-to-top"><i class="fa fa-angle-double-up"></i></a>
+    </body>
+</html>
