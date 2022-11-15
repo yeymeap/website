fd828854a398b475449caf3a64769bd72011c6bd
yeymeap <bence3728@gmail.com>
2022-11-15 18:37:08 +0100

    registering possible, not with email yet

--- sign-up-en.php
+++ sign-up-en.php
@@ -1,9 +1,137 @@
-<?php include 'topnav.php';
+<?php
+// Include config file
+require_once "config.php";
+ 
+// Define variables and initialize with empty values
+$username = $password = $confirm_password = $email = "";
+$username_err = $password_err = $confirm_password_err = $email_err = "";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    // Validate username
+    if(empty(trim($_POST["username"]))){
+        $username_err = "Please enter a username.";
+    } elseif(!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["username"]))){
+        $username_err = "Username can only contain letters, numbers, and underscores.";
+    } else{
+        // Prepare a select statement
+        $sql = "SELECT id FROM users WHERE username = ?";
+        
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_username);
+            
+            // Set parameters
+            $param_username = trim($_POST["username"]);
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                /* store result */
+                mysqli_stmt_store_result($stmt);
+                
+                if(mysqli_stmt_num_rows($stmt) == 1){
+                    $username_err = "This username is already taken.";
+                } else{
+                    $username = trim($_POST["username"]);
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
 
-include 'reg1.php';
-include 'menu.php';
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+    }
 
+    
+    // Validate password
+    if(empty(trim($_POST["password"]))){
+        $password_err = "Please enter a password.";     
+    } elseif(strlen(trim($_POST["password"])) < 6){
+        $password_err = "Password must have atleast 6 characters.";
+    } else{
+        $password = trim($_POST["password"]);
+    }
+    
+    // Validate confirm password
+    if(empty(trim($_POST["confirm_password"]))){
+        $confirm_password_err = "Please confirm password.";     
+    } else{
+        $confirm_password = trim($_POST["confirm_password"]);
+        if(empty($password_err) && ($password != $confirm_password)){
+            $confirm_password_err = "Password did not match.";
+        }
+    }
+    
+	// Validate email
+	/*if(empty(trim($_POST["email"]))){
+        $email_err = "Please enter an email.";
+	}
+	elseif(!filter_var($email, FILTER-VALIDATE-EMAIL)){
+		$email_err = 'Please enter a valid email';
+    } else{
+        // Prepare a select statement
+        $sql = "SELECT id FROM users WHERE email = ?";
+        
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "s", $param_email);
+            
+            // Set parameters
+            $param_email = trim($_POST["email"]);
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                /* store result *
+                mysqli_stmt_store_result($stmt);
+                
+                if(mysqli_stmt_num_rows($stmt) == 1){
+                    $email_err = "This email is already taken.";
+                } else{
+                    $email = trim($_POST["email"]);
+                }
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+    }*/
+
+    // Check input errors before inserting in database
+    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){ //&& empty($email_err)){
+        
+        // Prepare an insert statement
+        $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
+         
+        if($stmt = mysqli_prepare($link, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
+            
+            // Set parameters
+            $param_username = $username;
+            $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
+            //$param_email = $email;
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Redirect to login page
+                header("location: sign-in-en.php");
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+    }
+    
+    // Close connection
+    mysqli_close($link);
+}
 ?>
+
+<?php include 'topnav.php'; ?>
 
 		<!-- Breadcrumb Start -->
 		<div class="container-fluid">
@@ -22,117 +150,39 @@ include 'menu.php';
 		</div>
 		<!-- Breadcrumb End -->
 
-		<!-- Checkout Start -->
-    <form action="sign-up-en.php" method="POST">
-		<div class="container-fluid">
-			<div class="row px-xl-5 mb-5">
-				<div class="col-lg-12">
-					<h5
-						class="section-title position-relative text-uppercase mb-3">
-						<span class="bg-white pe-3">Billing Address</span>
-					</h5>
-					<div class="bg-white p-30">
-						<div class="row">
-							<div class="col-md-6 form-group mb-2">
-								<label for='user'>First Name</label>
-								<input
-									class="form-control mt-1"
-									type="text"
-									placeholder="Your First Name Here"
-                                    name='user' />
-							</div>
-							<div class="col-md-6 form-group mb-2">
-								<label>Last Name</label>
-								<input
-									class="form-control mt-1"
-									type="text"
-									placeholder="Your Last Name Here" />
-							</div>
-							<div class="col-md-6 form-group mb-2">
-								<label for="email">E-mail</label>
-								<input
-									class="form-control mt-1"
-									type="text"
-									placeholder="example@example.com"
-                                    name = "email"/>
-							</div>
-                            <div class="col-md-6 form-group mb-2">
-								<label for="pwd">Password</label>
-								<input
-									class="form-control mt-1"
-									type="password"
-									placeholder="Password Here"
-                                    name = "pwd"/>
-							</div>
-                            <div class="col-md-6 form-group mb-2">
-								<label for="pwdrepeat">Password Repeat</label>
-								<input
-									class="form-control mt-1"
-									type="password"
-									placeholder="Repeat Password"
-                                    name = "pwdrepeat"/>
-							</div>
-							<div class="col-md-6 form-group mb-2">
-								<label>Mobile Number</label>
-								<input
-									class="form-control mt-1"
-									type="text"
-									placeholder="+123 456 789" />
-							</div>
-							<div class="col-md-6 form-group mb-2">
-								<label>Address Line 1</label>
-								<input
-									class="form-control mt-1"
-									type="text"
-									placeholder="123 Street" />
-							</div>
-							<div class="col-md-6 form-group mb-2">
-								<label>Address Line 2</label>
-								<input
-									class="form-control mt-1"
-									type="text"
-									placeholder="123 Street" />
-							</div>
-
-							<div class="col-md-6 form-group mb-2">
-								<label>City</label>
-								<input
-									class="form-control mt-1"
-									type="text"
-									placeholder="New York" />
-							</div>
-							<div class="col-md-6 form-group mb-2">
-								<label>State</label>
-								<input
-									class="form-control mt-1"
-									type="text"
-									placeholder="New York" />
-							</div>
-							<div class="col-md-6 form-group mb-2">
-								<label>ZIP Code</label>
-								<input
-									class="form-control mt-1"
-									type="text"
-									placeholder="123" />
-							</div>
-							<div class="col-md-6 form-group mb-2">
-								<label>Country</label>
-								<div></div>
-								<select class="custom-select mt-2">
-									<option selected>Slovakia</option>
-									<option>Hungary</option>
-									<option>Czech Republic</option>
-									<option>Austria</option>
-									<option>Poland</option>
-									<option>Germany</option>
-								</select>
-							</div>
-                            <button type="submit" class="btn btn-black">Register</button> 
-						</div>
-					</div>
-				</div>
+	<div class="container">
+        <h2>Sign Up</h2>
+        <p>Please fill this form to create an account.</p>
+		<div class="col-lg-12">
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group mb-2 col-2">
+                <label>Username</label>
+                <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
+                <span class="invalid-feedback"><?php echo $username_err; ?></span>
+            </div>    
+            <div class="form-group mb-2 col-2">
+                <label>Password</label>
+                <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>">
+                <span class="invalid-feedback"><?php echo $password_err; ?></span>
             </div>
-        </div>
-    </form>
+            <div class="form-group mb-2 col-2">
+                <label>Confirm Password</label>
+                <input type="password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirm_password; ?>">
+                <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
+            </div>
+			<div class="form-group mb-2 col-2">
+                <label>Email</label>
+                <input type="text" name="email" class="form-control <?php echo (!empty($email_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $email; ?>">
+                <span class="invalid-feedback"><?php echo $email_err; ?></span>
+            </div>
+            <div class="form-group mb-2 col-2">
+                <input type="submit" class="btn btn-black" value="Submit">
+                <input type="reset" class="btn btn-white ms-2" value="Reset">
+            </div>
+            <p>Already have an account? <a href="sign-in-en.php">Login here</a>.</p>
+        </form>
+		</div>
+	</div>
+
 
 <?php include 'footer.php';?>
