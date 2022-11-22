56918b0f1fddf8f44a5857d377292d07e134838d
yeymeap <bence3728@gmail.com>
2022-11-22 07:35:04 +0100

    login and register possible with email

--- sign-up-en.php
+++ sign-up-en.php
@@ -1,40 +1,40 @@
 <?php
 // Include config file
 require_once "config.php";
- 
+
 // Define variables and initialize with empty values
 $username = $password = $confirm_password = $email = "";
 $username_err = $password_err = $confirm_password_err = $email_err = "";
- 
+
 // Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Validate username
-    if(empty(trim($_POST["username"]))){
+    if (empty(trim($_POST["username"]))) {
         $username_err = "Please enter a username.";
-    } elseif(!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["username"]))){
+    } elseif (!preg_match('/^[a-zA-Z0-9_]+$/', trim($_POST["username"]))) {
         $username_err = "Username can only contain letters, numbers, and underscores.";
-    } else{
+    } else {
         // Prepare a select statement
         $sql = "SELECT id FROM users WHERE username = ?";
-        
-        if($stmt = mysqli_prepare($link, $sql)){
+
+        if ($stmt = mysqli_prepare($link, $sql)) {
             // Bind variables to the prepared statement as parameters
             mysqli_stmt_bind_param($stmt, "s", $param_username);
-            
+
             // Set parameters
             $param_username = trim($_POST["username"]);
-            
+
             // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
+            if (mysqli_stmt_execute($stmt)) {
                 /* store result */
                 mysqli_stmt_store_result($stmt);
-                
-                if(mysqli_stmt_num_rows($stmt) == 1){
+
+                if (mysqli_stmt_num_rows($stmt) == 1) {
                     $username_err = "This username is already taken.";
-                } else{
+                } else {
                     $username = trim($_POST["username"]);
                 }
-            } else{
+            } else {
                 echo "Oops! Something went wrong. Please try again later.";
             }
 
@@ -43,81 +43,53 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         }
     }
 
-    
+
     // Validate password
-    if(empty(trim($_POST["password"]))){
-        $password_err = "Please enter a password.";     
-    } elseif(strlen(trim($_POST["password"])) < 6){
+    if (empty(trim($_POST["password"]))) {
+        $password_err = "Please enter a password.";
+    } elseif (strlen(trim($_POST["password"])) < 6) {
         $password_err = "Password must have atleast 6 characters.";
-    } else{
+    } else {
         $password = trim($_POST["password"]);
     }
-    
+
     // Validate confirm password
-    if(empty(trim($_POST["confirm_password"]))){
-        $confirm_password_err = "Please confirm password.";     
-    } else{
+    if (empty(trim($_POST["confirm_password"]))) {
+        $confirm_password_err = "Please confirm password.";
+    } else {
         $confirm_password = trim($_POST["confirm_password"]);
-        if(empty($password_err) && ($password != $confirm_password)){
+        if (empty($password_err) && ($password != $confirm_password)) {
             $confirm_password_err = "Password did not match.";
         }
     }
-    
-	// Validate email
-	/*if(empty(trim($_POST["email"]))){
+
+    // Validate email
+    if (empty(trim($_POST["email"]))) {
         $email_err = "Please enter an email.";
-	}
-	elseif(!filter_var($email, FILTER-VALIDATE-EMAIL)){
-		$email_err = 'Please enter a valid email';
-    } else{
+    } elseif (!filter_var(trim($_POST["email"]), FILTER_VALIDATE_EMAIL)) {
+        $email_err = 'Please enter a valid email';
+    } else {
         // Prepare a select statement
         $sql = "SELECT id FROM users WHERE email = ?";
-        
-        if($stmt = mysqli_prepare($link, $sql)){
+
+        if ($stmt = mysqli_prepare($link, $sql)) {
             // Bind variables to the prepared statement as parameters
             mysqli_stmt_bind_param($stmt, "s", $param_email);
-            
+
             // Set parameters
             $param_email = trim($_POST["email"]);
-            
+
             // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
-                /* store result *
+            if (mysqli_stmt_execute($stmt)) {
+                /* store result */
                 mysqli_stmt_store_result($stmt);
-                
-                if(mysqli_stmt_num_rows($stmt) == 1){
+
+                if (mysqli_stmt_num_rows($stmt) == 1) {
                     $email_err = "This email is already taken.";
-                } else{
+                } else {
                     $email = trim($_POST["email"]);
                 }
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
-            }
-
-            // Close statement
-            mysqli_stmt_close($stmt);
-        }
-    }*/
-
-    // Check input errors before inserting in database
-    if(empty($username_err) && empty($password_err) && empty($confirm_password_err)){ //&& empty($email_err)){
-        
-        // Prepare an insert statement
-        $sql = "INSERT INTO users (username, password) VALUES (?, ?)";
-         
-        if($stmt = mysqli_prepare($link, $sql)){
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password);
-            
-            // Set parameters
-            $param_username = $username;
-            $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
-            //$param_email = $email;
-            // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
-                // Redirect to login page
-                header("location: sign-in-en.php");
-            } else{
+            } else {
                 echo "Oops! Something went wrong. Please try again later.";
             }
 
@@ -125,7 +97,35 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             mysqli_stmt_close($stmt);
         }
     }
-    
+
+    // Check input errors before inserting in database
+    if (empty($username_err) && empty($password_err) && empty($confirm_password_err) && empty($email_err)) {
+
+        // Prepare an insert statement
+        $sql = "INSERT INTO users (username, password, email) VALUES (?, ?, ?)";
+
+        if ($stmt = mysqli_prepare($link, $sql)) {
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "sss", $param_username, $param_password, $param_email);
+
+            // Set parameters
+            $param_username = $username;
+            $param_password = password_hash($password, PASSWORD_DEFAULT); // Creates a password hash
+            $param_email = $email;
+
+            // Attempt to execute the prepared statement
+            if (mysqli_stmt_execute($stmt)) {
+                // Redirect to login page
+                header("location: sign-in-en.php");
+            } else {
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+
+            // Close statement
+            mysqli_stmt_close($stmt);
+        }
+    }
+
     // Close connection
     mysqli_close($link);
 }
@@ -133,33 +133,29 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
 <?php include 'topnav.php'; ?>
 
-		<!-- Breadcrumb Start -->
-		<div class="container-fluid">
-			<div class="row px-xl-5">
-				<div class="col-12">
-					<nav class="breadcrumb bg-white mb-30">
-						<a
-							class="breadcrumb-item text-dark"
-							href="index-en.php"
-							>Home</a
-						>
-						<span class="breadcrumb-item active">Register</span>
-					</nav>
-				</div>
-			</div>
-		</div>
-		<!-- Breadcrumb End -->
+<!-- Breadcrumb Start -->
+<div class="container-fluid">
+    <div class="row px-xl-5">
+        <div class="col-12">
+            <nav class="breadcrumb bg-white mb-30">
+                <a class="breadcrumb-item text-dark" href="index-en.php">Home</a>
+                <span class="breadcrumb-item active">Register</span>
+            </nav>
+        </div>
+    </div>
+</div>
+<!-- Breadcrumb End -->
 
-	<div class="container">
+<div class="container">
+    <div class="col-lg-12">
         <h2>Sign Up</h2>
         <p>Please fill this form to create an account.</p>
-		<div class="col-lg-12">
         <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
             <div class="form-group mb-2 col-2">
                 <label>Username</label>
                 <input type="text" name="username" class="form-control <?php echo (!empty($username_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $username; ?>">
                 <span class="invalid-feedback"><?php echo $username_err; ?></span>
-            </div>    
+            </div>
             <div class="form-group mb-2 col-2">
                 <label>Password</label>
                 <input type="password" name="password" class="form-control <?php echo (!empty($password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $password; ?>">
@@ -170,7 +166,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 <input type="password" name="confirm_password" class="form-control <?php echo (!empty($confirm_password_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $confirm_password; ?>">
                 <span class="invalid-feedback"><?php echo $confirm_password_err; ?></span>
             </div>
-			<div class="form-group mb-2 col-2">
+            <div class="form-group mb-2 col-2">
                 <label>Email</label>
                 <input type="text" name="email" class="form-control <?php echo (!empty($email_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $email; ?>">
                 <span class="invalid-feedback"><?php echo $email_err; ?></span>
@@ -181,8 +177,6 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             </div>
             <p>Already have an account? <a href="sign-in-en.php">Login here</a>.</p>
         </form>
-		</div>
-	</div>
-
-
-<?php include 'footer.php';?>
+    </div>
+</div>
+<?php include 'footer.php'; ?>
--- topnav.php
+++ topnav.php
@@ -1,134 +1,134 @@
 <!DOCTYPE html>
 <html lang="en-SK">
 
-    <head>
-        <meta charset="utf-8">
-        <title>GitárShop - Online Guitar Store</title>
-        <meta content="width=device-width, initial-scale=1.0" name="viewport">
-        <meta content="HangszerShop" name="keywords">
-        <meta content="Online Guitar Store" name="description">
+<head>
+    <meta charset="utf-8">
+    <title>GitárShop - Online Guitar Store</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="HangszerShop" name="keywords">
+    <meta content="Online Guitar Store" name="description">
 
-        <!-- Favicon -->
-        <link href="img/favicon.ico" rel="icon">
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
 
-        <!-- Google Web Fonts -->
-        <link rel="preconnect" href="https://fonts.gstatic.com">
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">  
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
 
-        <!-- Font Awesome -->
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" rel="stylesheet">
 
-        <!-- Libraries Stylesheet -->
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" rel="stylesheet">
-        <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+    <!-- Libraries Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
 
-        <!-- Customized Bootstrap Stylesheet -->
-        <link href="css/style.css" rel="stylesheet">
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
 
-        <!-- JavaScript Libraries -->
-        <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
-        <script src="lib/easing/easing.min.js"></script>
-        <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
 
-        <!-- Contact Javascript File -->
-        <script src="mail/jqBootstrapValidation.min.js"></script>
-        <script src="mail/contact.js"></script>
+    <!-- Contact Javascript File -->
+    <script src="mail/jqBootstrapValidation.min.js"></script>
+    <script src="mail/contact.js"></script>
 
-        <!-- Template Javascript -->
-        <script src="js/main.js"></script>
-    </head>
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
+</head>
 
-    <body>
-        <!-- Topbar Start -->
-        <div class="container-fluid">
-            <div class="row bg-light py-1 px-xl-5">
-                <div class="col-lg-6 d-none d-lg-block"></div>
-                <div class="col-lg-6 text-center text-lg-end">
-                    <div class="d-inline-flex align-items-center">
-                        <div class="dropdown-center mx-1">
-                            <button type="button" class="btn btn-sm btn-black dropdown-toggle" data-toggle="dropdown" aria-expanded="false">My Account</button>
-                            <ul class="dropdown-menu dropdown-menu-end min-width-account">
-                                <li><a class="dropdown-item text-decoration-none" href="sign-in-en.php">Sign in</a></li>
-                                <li><a class="dropdown-item text-decoration-none" href="sign-up-en.php">Sign up</a></li>
-                            </ul>
-                        </div>
-                        <div class="dropdown-center mx-1">
-                            <button type="button" class="btn btn-sm btn-black dropdown-toggle" data-toggle="dropdown" aria-expanded="false">English</button>
-                            <ul class="dropdown-menu dropdown-menu-end min-width-lang">
-                                <li><a class="dropdown-item text-decoration-none" href="#">EN</a></li>
-                                <li><a class="dropdown-item text-decoration-none" href="#">HU</a></li>
-                            </ul>
-                        </div>
+<body>
+    <!-- Topbar Start -->
+    <div class="container-fluid">
+        <div class="row bg-light py-1 px-xl-5">
+            <div class="col-lg-6 d-none d-lg-block"></div>
+            <div class="col-lg-6 text-center text-lg-end">
+                <div class="d-inline-flex align-items-center">
+                    <div class="dropdown-center mx-1">
+                        <button type="button" class="btn btn-sm btn-black dropdown-toggle" data-toggle="dropdown" aria-expanded="false">My Account</button>
+                        <ul class="dropdown-menu dropdown-menu-end min-width-account">
+                            <li><a class="dropdown-item text-decoration-none" href="sign-in-en.php">Sign in</a></li>
+                            <li><a class="dropdown-item text-decoration-none" href="sign-up-en.php">Sign up</a></li>
+                        </ul>
                     </div>
-                    <div class="d-inline-flex align-items-center d-block d-lg-none">
-                        <a href="" class="btn px-0 ms-2 text-decoration-none">
-                            <i class="fas fa-shopping-cart text-black"></i>
-                            <span class="badge text-black border border-black rounded-circle">0</span>
-                        </a>
+                    <div class="dropdown-center mx-1">
+                        <button type="button" class="btn btn-sm btn-black dropdown-toggle" data-toggle="dropdown" aria-expanded="false">English</button>
+                        <ul class="dropdown-menu dropdown-menu-end min-width-lang">
+                            <li><a class="dropdown-item text-decoration-none" href="#">EN</a></li>
+                            <li><a class="dropdown-item text-decoration-none" href="#">HU</a></li>
+                        </ul>
                     </div>
                 </div>
-            </div>
-            <div class="row align-items-center bg-light py-3 px-xl-5 d-none d-lg-flex">
-                <div class="col-lg-4">
-                    <a href="index-en.php" title="Back to main page" class="text-decoration-none">
-                        <span class="h1 text-uppercase text-white bg-black px-2">gitár shop</span>
+                <div class="d-inline-flex align-items-center d-block d-lg-none">
+                    <a href="" class="btn px-0 ms-2 text-decoration-none">
+                        <i class="fas fa-shopping-cart text-black"></i>
+                        <span class="badge text-black border border-black rounded-circle">0</span>
                     </a>
                 </div>
-                <div class="col-lg-4 col-6 text-start">
-                    <form action="">
-                        <div class="input-group">
-                            <input type="text" class="form-control" placeholder="Search for products">
-                            <div class="input-group-append">
-                                <a href="#" title="Search" class="text-decoration-none" >
-                                    <span class="input-group-text bg-transparent text-black pt-3">
-                                        <i class="fa fa-search mb-2"></i>
-                                    </span>
-                                </a>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-                <div class="col-lg-4 col-6 text-end">                  
-                    <p class="m-0 text-dark">Customer Service</p>
-                    <h5 class="m-0 tadaer">+421 949 131 222</h5>
-                </div>
             </div>
         </div>
-        <!-- Topbar End -->
-
-
-        <!-- Navbar Start -->
-        <div class="container-fluid bg-black mb-30">
-            <div class="row px-xl-5">
-                <div class="col-lg-12">
-                    <nav class="navbar navbar-expand-lg bg-black navbar-dark py-3 py-lg-0 px-0">
-                        <a href="" class="text-decoration-none d-block d-lg-none">
-                            <span class="h1 text-uppercase text-white bg-black px-2">Gitár Shop</span>
-                        </a>
-                        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
-                            <span class="navbar-toggler-icon"></span>
-                        </button>
-                        <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
-                            <div class="navbar-nav me-4 py-0">
-                                <a href="guitars-en.html" class="nav-item nav-link text-decoration-none">Shop Guitars</a>
-                                <a href="bass-en.html" class="nav-item nav-link text-decoration-none">Shop Bass Guitars</a>
-                                <a href="amplifiers-en.html" class="nav-item nav-link text-decoration-none">Shop Amplifiers</a>
-                                <a href="misc-en.html" class="nav-item nav-link text-decoration-none">Shop Misc</a>
-                            </div>
-                            <div class="navbar-nav py-0">
-                                <a href="guides-en.php" class="nav-item nav-link text-decoration-none">Beginner's Guides</a>
-                                <a href="contact-en.php" class="nav-item nav-link text-decoration-none">Contact</a>
-                            </div>
-                            <div class="navbar-nav ms-auto py-0 d-none d-lg-block">
-                                <a href="cart-en.php" class="btn px-0 ms-3 text-decoration-none">
-                                    <i class="fas fa-shopping-cart text-white"></i>
-                                    <span class="badge text-light border border-white rounded-circle">0</span>
-                                </a>
-                            </div>
+        <div class="row align-items-center bg-light py-3 px-xl-5 d-none d-lg-flex">
+            <div class="col-lg-4">
+                <a href="index-en.php" title="Back to main page" class="text-decoration-none">
+                    <span class="h1 text-uppercase text-white bg-black px-2">gitár shop</span>
+                </a>
+            </div>
+            <div class="col-lg-4 col-6 text-start">
+                <form action="">
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Search for products">
+                        <div class="input-group-append">
+                            <a href="#" title="Search" class="text-decoration-none">
+                                <span class="input-group-text bg-transparent text-black pt-3">
+                                    <i class="fa fa-search mb-2"></i>
+                                </span>
+                            </a>
                         </div>
-                    </nav>
-                </div>
+                    </div>
+                </form>
+            </div>
+            <div class="col-lg-4 col-6 text-end">
+                <p class="m-0 text-dark">Customer Service</p>
+                <h5 class="m-0 tadaer">+421 949 131 222</h5>
             </div>
         </div>
-        <!-- Navbar End -->
+    </div>
+    <!-- Topbar End -->
+
+
+    <!-- Navbar Start -->
+    <div class="container-fluid bg-black mb-30">
+        <div class="row px-xl-5">
+            <div class="col-lg-12">
+                <nav class="navbar navbar-expand-lg bg-black navbar-dark py-3 py-lg-0 px-0">
+                    <a href="" class="text-decoration-none d-block d-lg-none">
+                        <span class="h1 text-uppercase text-white bg-black px-2">Gitár Shop</span>
+                    </a>
+                    <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse justify-content-between" id="navbarCollapse">
+                        <div class="navbar-nav me-4 py-0">
+                            <a href="guitars-en.html" class="nav-item nav-link text-decoration-none">Shop Guitars</a>
+                            <a href="bass-en.html" class="nav-item nav-link text-decoration-none">Shop Bass Guitars</a>
+                            <a href="amplifiers-en.html" class="nav-item nav-link text-decoration-none">Shop Amplifiers</a>
+                            <a href="misc-en.html" class="nav-item nav-link text-decoration-none">Shop Misc</a>
+                        </div>
+                        <div class="navbar-nav py-0">
+                            <a href="guides-en.php" class="nav-item nav-link text-decoration-none">Beginner's Guides</a>
+                            <a href="contact-en.php" class="nav-item nav-link text-decoration-none">Contact</a>
+                        </div>
+                        <div class="navbar-nav ms-auto py-0 d-none d-lg-block">
+                            <a href="cart-en.php" class="btn px-0 ms-3 text-decoration-none">
+                                <i class="fas fa-shopping-cart text-white"></i>
+                                <span class="badge text-light border border-white rounded-circle">0</span>
+                            </a>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
+    <!-- Navbar End -->
