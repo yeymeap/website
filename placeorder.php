916791b844a6853170b0c4094848cd453e8b8c4e
yeymeap <bence3728@gmail.com>
2023-06-14 14:51:21 +0200

    order placement

--- placeorder.php
+++ placeorder.php
@@ -11,15 +11,28 @@ if (!isset($_SESSION['authenticate'])) {
     //echo "You came from checkout.php!";
     $_SESSION['authenticate'] = null;
     include 'topnav.php';
-    echo
-    "<div class='container-fluid'>
-    <div class='col-12'>
-        <div class='my-5'>
-            <div class='h1 text-black d-flex justify-content-center'>Your Order Has Been Placed</div><br>
-            <div class='h2 text-black-50 d-flex justify-content-center'>Thank you for ordering from us, we'll contact you by email with your order details!</div>
+    if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
+        echo
+        "<div class='container-fluid'>
+        <div class='col-12'>
+            <div class='my-5'>
+                <div class='h1 text-black d-flex justify-content-center'>Your Order Has Been Placed</div><br>
+                <div class='h2 text-black-50 d-flex justify-content-center'>Thank you for ordering from us, we'll contact you by email with your order details!</div>
+            </div>
         </div>
     </div>
-</div>
-</div>";
+    </div>";
+    } else {
+        echo
+        "<div class='container-fluid'>
+        <div class='col-12'>
+            <div class='my-5'>
+                <div class='h1 text-black d-flex justify-content-center'>Your Order Has Been Placed, $login</div><br>
+                <div class='h2 text-black-50 d-flex justify-content-center'>Thank you for ordering from us, we'll contact you by email with your order details!</div>
+            </div>
+        </div>
+    </div>
+    </div>";
+    }
     include 'footer.php';
 }
